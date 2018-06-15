65ad8884d01e6e0b9d9db776700ccd26b0ae30a1
Igor_Levin <ilevin.office@gmail.com>
2018-06-15 21:40:08 +0300

    Message server PID logging

--- L16-msgserver/src/main/java/ru/otus/l16/ServerMain.java
+++ L16-msgserver/src/main/java/ru/otus/l16/ServerMain.java
@@ -1,22 +1,23 @@
 package ru.otus.l16;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.otus.l16.server.SocketMsgServer;
 
 import javax.management.MBeanServer;
 import javax.management.ObjectName;
-import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 /**
  * Created by tully.
  */
 public class ServerMain {
-    private static final Logger logger = Logger.getLogger(ServerMain.class.getName());
+
+    private static final Logger log = LoggerFactory.getLogger(ServerMain.class);
 
     //private static final String CLIENT_START_COMMAND = "java -jar ../L16.1.2-client/target/client.jar";
     private static final int CLIENT_START_DELAY_SEC = 5;
@@ -26,6 +27,8 @@ public class ServerMain {
     }
 
     private void start() throws Exception {
+
+        log.info("PID: {}", ManagementFactory.getRuntimeMXBean().getName());
 
         ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
         startClient(executorService);
