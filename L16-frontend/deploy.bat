666eb3d581e2efa0500d24c44a916e921d9144d9
Igor <igorjnay@yandex.ru>
2018-06-17 19:51:10 +0300

    Недостающие файлы

---
+++ L16-frontend/deploy.bat
@@ -0,0 +1,2 @@
+copy /Y .\target\L16-frontend.war %JETTY_HOME%\webapps\root.war
+
---
+++ L16-frontend/public_html/js/cache_state.js
@@ -0,0 +1,17 @@
+function setCacheState(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === XMLHttpRequest.DONE && this.status == 200) {
+            document.getElementById('cacheCapacity').innerHTML = JSON.parse(this.responseText).capacity;
+            document.getElementById('cacheSize').innerHTML = JSON.parse(this.responseText).size;
+            document.getElementById('miss').innerHTML = JSON.parse(this.responseText).miss;
+            document.getElementById('hit').innerHTML = JSON.parse(this.responseText).hit;
+        }
+    };
+    xhttp.open("GET", "/cache_state", true);
+    xhttp.send();
+}
+
+function refresh() {
+    setCacheState();
+}
